549af579ba1ee024731b8468cfbf30b817237de6
vaibhaviprabhu-15 <vaibhaviprabhu707@gmail.com>
2025-12-02 12:08:04 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,15 @@
+import pytest
+def student_details(id,name,course,year):
+    result = (
+        f"Student ID: {id}\n"
+        f"Student name: {name}\n"
+        f"Course: {course}\n"
+        f"Year: {year}\n"
+    )
+    return result 
+if __name__=="__main__":
+    id="101"
+    name="vaibhavi"
+    course="bca"
+    year=2024
+    print(student_details(id,name,course,year))
